63c18a3856cb67e933e9953360069ffc21146d66
Marco HC <marcohernaizcao@gmail.com>
2019-12-22 12:31:25 +0100

    Added OneTimeExecutable

---
+++ core-mvi/src/main/kotlin/com/marcohc/terminator/core/mvi/ui/consumable/OneTimeExecutable.kt
@@ -0,0 +1,22 @@
+package com.marcohc.terminator.core.mvi.ui.consumable
+
+import java.util.concurrent.atomic.AtomicReference
+
+/**
+ * Meant for UI actions which are executed only once
+ */
+class OneTimeExecutable private constructor(value: Unit? = null) {
+
+    private val wrapperAtomicReference: AtomicReference<Unit?> = AtomicReference(value)
+
+    fun execute(function: () -> Unit) {
+        wrapperAtomicReference.getAndSet(null)?.let { function.invoke() }
+    }
+
+    companion object {
+        fun load() = OneTimeExecutable(Unit)
+
+        fun empty() = OneTimeExecutable()
+    }
+
+}
