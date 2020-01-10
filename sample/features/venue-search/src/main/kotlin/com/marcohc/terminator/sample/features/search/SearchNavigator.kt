dd2c18736a99408127179f47a3c8fbe132588291
Marco HC <marcohernaizcao@gmail.com>
2020-01-10 15:24:23 +0100

    Removed sample folder

--- sample/features/venue-search/src/main/kotlin/com/marcohc/terminator/sample/features/search/SearchNavigator.kt
+++
@@ -1,9 +0,0 @@
-package com.marcohc.terminator.sample.features.search
-
-import android.app.Activity
-
-interface SearchNavigator {
-
-    fun goToVenue(activity: Activity, requestCode: Int, id: String)
-
-}
