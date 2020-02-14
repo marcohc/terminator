532ce7c01e58118af1de44a88dc75e41893b3c7d
Marco HC <marcohernaizcao@gmail.com>
2020-02-14 17:18:29 +0100

    Added contextExt methods

--- core-utils/src/main/kotlin/com/marcohc/terminator/core/utils/contextExt.kt
+++ core-utils/src/main/kotlin/com/marcohc/terminator/core/utils/contextExt.kt
@@ -1,9 +1,20 @@
 package com.marcohc.terminator.core.utils
 
 import android.content.Context
+import android.util.TypedValue
 import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
 import androidx.core.content.ContextCompat
 
 @ColorInt
 fun Context.getColorCompat(@ColorRes colorRes: Int): Int = ContextCompat.getColor(this, colorRes)
+
+fun Context.getColorCompatFromAttribute(attributeId: Int): Int {
+    return getColorCompat(getColorFromAttribute(attributeId))
+}
+
+fun Context.getColorFromAttribute(attributeId: Int): Int {
+    val typedValue = TypedValue()
+    theme.resolveAttribute(attributeId, typedValue, true)
+    return typedValue.resourceId
+}
