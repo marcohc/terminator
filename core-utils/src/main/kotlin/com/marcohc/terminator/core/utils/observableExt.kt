f2f8348b508961a50364e75bcda32302a3254121
Marco HC <marcohernaizcao@gmail.com>
2020-06-12 11:01:26 +0200

    Remove "org.intellij.lang.annotations" from Timber

--- core-utils/src/main/kotlin/com/marcohc/terminator/core/utils/observableExt.kt
+++ core-utils/src/main/kotlin/com/marcohc/terminator/core/utils/observableExt.kt
@@ -3,7 +3,10 @@
 package com.marcohc.terminator.core.utils
 
 import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.functions.BiFunction
+
+fun <T> Observable<T>.toSingle(): Single<T> = take(1).singleOrError()
 
 fun <T> T.observableJust() = Observable.just(this)
 
